29db965ac627973bf87b291bd3724467937e1c3f
TimoxaV <vadim2493@gmail.com>
2020-10-15 13:28:45 +0300

    added logger; change system.out to logger in main

--- src/main/java/com/cinema/Main.java
+++ src/main/java/com/cinema/Main.java
@@ -16,8 +16,10 @@ import com.cinema.service.OrderService;
 import com.cinema.service.ShoppingCartService;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import org.apache.log4j.Logger;
 
 public class Main {
+    private static Logger logger = Logger.getLogger(Main.class);
     private static Injector injector = Injector.getInstance("com.cinema");
 
     public static void main(String[] args) throws AuthenticationException {
@@ -25,23 +27,23 @@ public class Main {
         fastAndFurious.setTitle("Fast and Furious");
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
         movieService.add(fastAndFurious);
-        movieService.getAll().forEach(System.out::println);
+        movieService.getAll().forEach(logger::info);
 
         Movie sully = new Movie();
         sully.setTitle("Sully");
         movieService.add(sully);
-        movieService.getAll().forEach(System.out::println);
+        movieService.getAll().forEach(logger::info);
 
-        System.out.println("------------------Cinema Halls--------------------------");
+        logger.info("------------------Cinema Halls--------------------------");
         CinemaHall cinemaHall = new CinemaHall();
         cinemaHall.setCapacity(150);
         cinemaHall.setDescription("Simple hall");
         CinemaHallService cinemaHallService
                 = (CinemaHallService) injector.getInstance(CinemaHallService.class);
         cinemaHallService.add(cinemaHall);
-        cinemaHallService.getAll().forEach(System.out::println);
+        cinemaHallService.getAll().forEach(logger::info);
 
-        System.out.println("------------------Movie Sessions--------------------------");
+        logger.info("------------------Movie Sessions--------------------------");
         MovieSession sessionFastAndFurious1 = new MovieSession();
         sessionFastAndFurious1.setCinemaHall(cinemaHall);
         sessionFastAndFurious1.setMovie(fastAndFurious);
@@ -68,25 +70,25 @@ public class Main {
         sessionFastAndFurious2.setShowTime(LocalDateTime.now().minusDays(2));
         sessionService.add(sessionFastAndFurious2);
 
-        System.out.println("------------------Find Fast And Furious Sessions--------------");
+        logger.info("------------------Find Fast And Furious Sessions--------------");
         sessionService.findAvailableSessions(fastAndFurious.getId(),
                 LocalDate.now())
-                .forEach(System.out::println);
+                .forEach(logger::info);
         System.out.println("------------------Find Sully Sessions--------------------------");
         sessionService.findAvailableSessions(sully.getId(), LocalDate.now())
-                .forEach(System.out::println);
+                .forEach(logger::info);
 
-        System.out.println("------------------Users Check--------------------------");
+        logger.info("------------------Users Check--------------------------");
         AuthenticationService authService =
                 (AuthenticationService) injector.getInstance(AuthenticationService.class);
         authService.register("new1@gmail.com", "1234");
         authService.register("new2@gmail.com", "1234");
         User new1 = authService.login("new1@gmail.com", "1234");
         User new2 = authService.login("new2@gmail.com", "1234");
-        System.out.println(new1);
-        System.out.println(new2);
+        logger.info(new1);
+        logger.info(new2);
 
-        System.out.println("------------------ShoppingCarts Check--------------------------");
+        logger.info("------------------ShoppingCarts Check--------------------------");
         ShoppingCartService cartService =
                 (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
         cartService.addSession(sessionFastAndFurious1, new1);
@@ -98,14 +100,14 @@ public class Main {
         cartService.addSession(sessionSully2, new2);
         ShoppingCart cart2 = cartService.getByUser(new2);
 
-        System.out.println("------------------Orders Check--------------------------");
+        logger.info("------------------Orders Check--------------------------");
         OrderService orderService =
                 (OrderService) injector.getInstance(OrderService.class);
         Order order1 = orderService.completeOrder(cart1.getTickets(), cart1.getUser());
-        System.out.println(order1);
-        System.out.println(cartService.getByUser(new1));
+        logger.info(order1);
+        logger.info(cartService.getByUser(new1));
         Order order2 = orderService.completeOrder(cart2.getTickets(), cart2.getUser());
-        System.out.println(order2);
-        System.out.println(orderService.getOrderHistory(new1));
+        logger.info(order2);
+        logger.info(orderService.getOrderHistory(new1));
     }
 }
